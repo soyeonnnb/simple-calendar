c8105ab38e36616f994bbfb8e96baa808a22e850
soyeonnnb <la28s5d@naver.com>
2024-10-28 21:51:27 +0900

    spring init

---
+++ BE/simplecalendar/src/main/java/com/simplest/simplecalendar/SimplecalendarApplication.java
@@ -0,0 +1,13 @@
+package com.simplest.simplecalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplecalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplecalendarApplication.class, args);
+	}
+
+}
---
+++ BE/simplecalendar/src/test/java/com/simplest/simplecalendar/SimplecalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplest.simplecalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplecalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
